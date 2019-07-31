36a08012d5f66f6c136d0b59cf146c123c0a5aac
hariharan.s <hariharan.s@zohocorp.com>
2019-08-01 00:47:46 +0530

    First phase of App done

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+import _ from 'lodash';
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <h3>
+          Edit a Stream
+        </h3>
+        <StreamForm 
+          initialValues={_.pick(this.props.stream, 'title', 'description')} 
+          onSubmit={(formValues) => this.props.editStream(this.props.match.params.id, formValues)} 
+        />
+      </div>
+    );
+  }
 };
-
-export default StreamEdit;
+const mapStateToProps = (state, {match: {params: {id}}}) => {
+  return {stream: state.streams[id]}
+}
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 import { reducer as formRedcuer } from 'redux-form';
 export default combineReducers({
   auth: authReducer,
-  form: formRedcuer
+  form: formRedcuer,
+  streams: streamReducer
 })
